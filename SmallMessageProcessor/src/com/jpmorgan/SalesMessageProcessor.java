94469ef2193549a5167ae93d99d6899c345fac1a
shilpar87 <55799786+shilpar87@users.noreply.github.com>
2019-09-29 19:12:02 +0530

    Message Processing initial commit

---
+++ SmallMessageProcessor/src/com/jpmorgan/SalesMessageProcessor.java
@@ -0,0 +1,33 @@
+package com.jpmorgan;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import com.jpmorgan.processor.Sale;
+
+public class SalesMessageProcessor {
+	public static void main(String[] args) {
+		// Initiate the sale object
+		Sale sale = new Sale();
+
+		// Read inputs from test file
+		try {
+			String line;
+			BufferedReader inputFile = new BufferedReader(new FileReader("testInput/input.txt"));
+			while ((line = inputFile.readLine()) != null) {
+				// process message for each sale notification
+				sale.processNotification(line);
+
+				// Call the report
+				// @note: report only generates after every 10th iteration and stops on 50th
+				// iteration and pauses for
+				// 2 seconds.
+				sale.log.report();
+			}
+		}catch (java.io.FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (java.io.IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
